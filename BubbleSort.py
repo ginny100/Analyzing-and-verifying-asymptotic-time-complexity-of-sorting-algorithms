30687fceff66e2f18ba9b1714da1b0175ba896b3
Ginny (Khue) Dang <58489398+ginny100@users.noreply.github.com>
2020-10-11 16:35:08 -0500

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,10 +1,9 @@
-# Function to do insertion sort 
-def insertionSort(arr): 
-    for i in range(1, len(arr)): 
-        key = arr[i] 
-        # Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position 
-        j = i - 1
-        while j >= 0 and key < arr[j]: 
-                arr[j + 1] = arr[j] 
-                j -= 1
-        arr[j + 1] = key 
+# Function to do bubble sort 
+
+def bubbleSort(myList): # Bubble Sort Algorithm
+    for i in range(len(myList)-1):
+        for j in range(len(myList)-i-1):
+            if myList[j] > myList[j + 1]:
+                myList[j], myList[j+1] = \
+                           myList[j + 1], myList[j]
+    return myList
