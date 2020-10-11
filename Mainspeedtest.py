c844ece091046e6ca8bffae9b1ad33610f6a20f4
christianhall058 <71720978+christianhall058@users.noreply.github.com>
2020-10-11 13:02:39 -0500

    Add files via upload
    Changed the quicksort to iterative quicksort and optimized everything

--- Mainspeedtest.py
+++ Mainspeedtest.py
@@ -50,34 +50,69 @@ def mergeSort(arr):
             j+= 1
             k+= 1
 
-def partition(arr,low,high): 
-    i = ( low-1 )         # index of smaller element 
-    pivot = arr[high]     # pivot 
+# This code is contributed by Mohit Kumra
+# This function is same in both iterative and recursive 
+def partition(arr, l, h): 
+    i = ( l - 1 ) 
+    x = arr[h] 
   
-    for j in range(low , high): 
+    for j in range(l, h): 
+        if   arr[j] <= x: 
   
-        # If current element is smaller than the pivot 
-        if   arr[j] < pivot: 
-          
             # increment index of smaller element 
-            i = i+1 
-            arr[i],arr[j] = arr[j],arr[i] 
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i] 
   
-    arr[i+1],arr[high] = arr[high],arr[i+1] 
-    return ( i+1 ) 
+    arr[i + 1], arr[h] = arr[h], arr[i + 1] 
+    return (i + 1) 
   
 # Function to do Quick sort 
-def quickSort(arr,low,high): 
-    if low < high: 
+# arr[] --> Array to be sorted, 
+# l  --> Starting index, 
+# h  --> Ending index 
+def quickSortIterative(arr, l, h): 
   
-        # pi is partitioning index, arr[p] is now 
-        # at right place 
-        pi = partition(arr,low,high) 
+    # Create an auxiliary stack 
+    size = h - l + 1
+    stack = [0] * (size) 
   
-        # Separately sort elements before 
-        # partition and after partition 
-        quickSort(arr, low, pi-1) 
-        quickSort(arr, pi+1, high) 
+    # initialize top of stack 
+    top = -1
+  
+    # push initial values of l and h to stack 
+    top = top + 1
+    stack[top] = l 
+    top = top + 1
+    stack[top] = h 
+  
+    # Keep popping from stack while is not empty 
+    while top >= 0: 
+  
+        # Pop h and l 
+        h = stack[top] 
+        top = top - 1
+        l = stack[top] 
+        top = top - 1
+  
+        # Set pivot element at its correct position in 
+        # sorted array 
+        p = partition( arr, l, h ) 
+  
+        # If there are elements on left side of pivot, 
+        # then push left side to stack 
+        if p-1 > l: 
+            top = top + 1
+            stack[top] = l 
+            top = top + 1
+            stack[top] = p - 1
+  
+        # If there are elements on right side of pivot, 
+        # then push right side to stack 
+        if p + 1 < h: 
+            top = top + 1
+            stack[top] = p + 1
+            top = top + 1
+            stack[top] = h 
 
 # This code is contributed by Mohit Kumra        
 def insertionSort(arr): 
@@ -110,24 +145,29 @@ def truncate(number, decimals=0):
     factor = 10.0 ** decimals
     return math.trunc(number * factor) / factor
 
-w = open('algtest.txt', 'w')
+w = open('algtest8.txt', 'w')
+w.truncate(0)
 
-sorted_list = list(range(1, 10000))
+arraynum = 100000
+testloopnum = 10
+
+sorted_list = list(range(1, arraynum))
 sorted_listcopy = sorted_list.copy()
 
-reverse_list = sorted_list.copy()
-reverse_list.reverse()
-reverse_listcopy = reverse_list.copy()
+# reverse_list = sorted_list.copy()
+# reverse_list.reverse()
+# reverse_listcopy = reverse_list.copy()
 
 ave_list = []
-for i in range(100):
+for i in range(testloopnum):
     ave_list.append([])
-for i in range(100):
-    for j in range(10000):
+for i in range(testloopnum):
+    for j in range(1, arraynum):
         ave_list[i].append(j)
-for i in range(100):
+for i in range(testloopnum):
     random.shuffle(ave_list[i])
 
+w.write("For " + str(testloopnum) + " loops and " + str(arraynum) + " array elements\n\n")
 
 totalb = 0
 averageb = 0
@@ -136,7 +176,7 @@ averagea = 0
 totalw = 0
 averagew = 0
 
-for x in range(1):
+for x in range(testloopnum):
     #B best case is sorted
     
     sorted_list = sorted_listcopy.copy()
@@ -144,7 +184,7 @@ for x in range(1):
     ave_listb = ave_list[x].copy()
     
     #B worst case is reverse sorted
-    reverse_list = reverse_listcopy.copy()
+    # reverse_list = reverse_listcopy.copy()
     
     start = time.perf_counter_ns()
     bubbleSort(sorted_list)
@@ -162,20 +202,20 @@ for x in range(1):
     #print("Bubble sort completed " + str(duration) + " seconds")
     totala = totala + duration
     
-    start = time.perf_counter_ns()
-    bubbleSort(reverse_list)
-    stop = time.perf_counter_ns()
-    #print(worst_list)
-    duration = (stop - start)
-    #print("Bubble sort completed " + str(duration) + " seconds")
-    totalw = totalw + duration
+    # start = time.perf_counter_ns()
+    # bubbleSort(reverse_list)
+    # stop = time.perf_counter_ns()
+    # #print(worst_list)
+    # duration = (stop - start)
+    # #print("Bubble sort completed " + str(duration) + " seconds")
+    # totalw = totalw + duration
    
-averageb = truncate(((totalb/100)* 10**-9), 7)
-w.write("Bubble sort best time complexity average is: " + str(averageb) + " seconds")
-averagea = truncate(((totala/100)* 10**-9), 7)
-w.write("Bubble sort average time complexity is: " + str(averagea) + " seconds")
-averagew = truncate(((totalw/100)* 10**-9), 7)
-w.write("Bubble sort worst time complexity average is: " + str(averagew) + " seconds") 
+averageb = truncate(((totalb/testloopnum)* 10**-9), 7)
+w.write("Bubble sort best time complexity average is: " + str(averageb) + " seconds\n")
+averagea = truncate(((totala/testloopnum)* 10**-9), 7)
+w.write("Bubble sort average time complexity is: " + str(averagea) + " seconds\n\n")
+# averagew = truncate(((totalw/testloopnum)* 10**-9), 7)
+# w.write("Bubble sort worst time complexity average is: " + str(averagew) + " seconds\n\n") 
    
 totalb = 0
 averageb = 0
@@ -184,7 +224,7 @@ averagea = 0
 totalw = 0
 averagew = 0
 
-for x in range(100):
+for x in range(testloopnum):
     
     ave_listm = ave_list[x].copy()
     
@@ -196,8 +236,8 @@ for x in range(100):
     #print("Merge sort completed " + str(duration) + " seconds")
     totala = totala + duration
 
-averagea = truncate(((totala/100)* 10**-9), 7)
-w.write("Merge sort average time complexity is: " + str(averagea) + " seconds")
+averagea = truncate(((totala/testloopnum)* 10**-9), 7)
+w.write("Merge sort average time complexity is: " + str(averagea) + " seconds\n\n")
 
 totalb = 0
 averageb = 0
@@ -206,9 +246,9 @@ averagea = 0
 totalw = 0
 averagew = 0
     
-for x in range(100):
+for x in range(testloopnum):
     low = 0
-    high = 9998
+    high = arraynum - 2
     
     #Q average case is random array
     ave_listq = ave_list[x].copy()
@@ -217,7 +257,7 @@ for x in range(100):
     worst_list = sorted_listcopy.copy()
     
     start = time.perf_counter_ns()
-    quickSort(ave_listq, low, high)
+    quickSortIterative(ave_listq, low, high)
     stop = time.perf_counter_ns()
     #print(ave_list)
     duration = (stop - start)
@@ -225,17 +265,17 @@ for x in range(100):
     totala = totala + duration
     
     start = time.perf_counter_ns()
-    quickSort(worst_list, low, high)
+    quickSortIterative(worst_list, low, high)
     stop = time.perf_counter_ns()
     #print(worst_list)
     duration = (stop - start)
     #print("Quick sort completed " + str(duration) + " seconds")
     totalw = totalw + duration
 
-averagea = truncate(((totala/100)* 10**-9), 7)
-w.write("Quick sort average time complexity is: " + str(averagea) + " seconds")
-averagew = truncate(((totalw/100)* 10**-9), 7)
-w.write("Quick sort worst time complexity average is: " + str(averagew) + " seconds")
+averagea = truncate(((totala/testloopnum)* 10**-9), 7)
+w.write("Quick sort average time complexity is: " + str(averagea) + " seconds\n")
+averagew = truncate(((totalw/testloopnum)* 10**-9), 7)
+w.write("Quick sort worst time complexity average is: " + str(averagew) + " seconds\n\n")
 
 totalb = 0
 averageb = 0
@@ -244,7 +284,7 @@ averagea = 0
 totalw = 0
 averagew = 0    
 
-for x in range(1):
+for x in range(testloopnum):
       
     #I best case is sorted
     best_list = sorted_listcopy.copy()
@@ -267,9 +307,11 @@ for x in range(1):
     #print("Insertion sort completed " + str(duration) + " seconds")
     totala = totala + duration
     
-averageb = truncate(((totalb/100)* 10**-9), 7)
-w.write("Insertion sort best time complexity average is: " + str(averageb) + " seconds")
-averagea = truncate(((totala/100)* 10**-9), 7)
-w.write("Insertion sort average time complexity is: " + str(averagea) + " seconds")
+averageb = truncate(((totalb/testloopnum)* 10**-9), 7)
+w.write("Insertion sort best time complexity average is: " + str(averageb) + " seconds\n")
+averagea = truncate(((totala/testloopnum)* 10**-9), 7)
+w.write("Insertion sort average time complexity is: " + str(averagea) + " seconds\n\n")
+
+w.close()
 
 # duration = truncate(((stop - start)* 10**-9), 7)
